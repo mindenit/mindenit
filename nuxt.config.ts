fe627c046dcf5f8fbd8db87aeae1e1c4b83c113d
perkinson1251 <tttrashutin@gmail.com>
2025-06-03 23:31:34 +0300

    feat: create layers and add color mode module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,31 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-05-15',
 	devtools: { enabled: true },
+	extends: ['./core', './layers/site'],
+	imports: {
+		dirs: [
+			'./core/types',
+			'./core/constants',
+			'./layers/**/types',
+			'./layers/**/schemas',
+			'./layers/**/queries',
+		],
+	},
+	future: {
+		compatibilityVersion: 4,
+	},
 
-	modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
+	modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxtjs/color-mode'],
+	icon: {
+		provider: 'iconify',
+		serverBundle: {
+			collections: ['lucide'],
+		},
+	},
+	colorMode: {
+		preference: 'system',
+		fallback: 'light',
+		classSuffix: '',
+		storageKey: 'nuxt-color-mode',
+	},
 })
