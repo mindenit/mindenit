ebf8002187940847c381b00e20bb54742301c0ec
perkinson1251 <tttrashutin@gmail.com>
2025-06-03 22:46:11 +0300

    chore: add basic lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,19 @@
-// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
-export default withNuxt()
+export default withNuxt([
+	{
+		// Конфігурація для інтеграції Prettier
+		plugins: {
+			prettier: prettierPlugin,
+		},
+		rules: {
+			'prettier/prettier': 'error',
+		},
+	},
+	{
+		ignores: ['**/*.md', 'pnpm-lock.yaml', '.env', '.env.*', '.nuxt', 'dist', 'node_modules'],
+	},
+	prettierConfig,
+])
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
+	compatibilityDate: '2025-05-15',
+	devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
+	modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
 })
