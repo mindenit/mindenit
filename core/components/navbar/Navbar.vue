70347c40b79c4aeb923c4ed8f05e4449bff1e047
perkinson1251 <tttrashutin@gmail.com>
2025-06-22 02:00:25 +0300

    feat: add hiring link to navbar

--- core/components/navbar/Navbar.vue
+++ core/components/navbar/Navbar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { onClickOutside } from '@vueuse/core'
+import { navigationItems, socialLinks } from '~/core/constants'
 
 const showMobileMenu = ref(false)
 const navbarRef = ref()
@@ -16,61 +17,6 @@ const handleMobileMenuToggle = () => {
 const handleMobileMenuClose = () => {
 	showMobileMenu.value = false
 }
-
-const navigationItems: NavigationItem[] = [
-	{
-		label: 'Головна',
-		href: '/',
-	},
-	{
-		label: 'Документи',
-		children: [
-			{
-				label: 'Меморандум',
-				href: '/memorandum',
-			},
-		],
-	},
-	{
-		label: 'Програми',
-		children: [
-			{
-				label: 'Student Program',
-				href: '/student-program',
-			},
-			{
-				label: 'Developer Program',
-				href: '/developer-program',
-			},
-		],
-	},
-]
-
-const socialLinks: SocialLink[] = [
-	{
-		name: 'Telegram',
-		href: 'https://t.me/mindenit',
-		icon: 'streamline-logos:telegram-logo-2-solid',
-		hoverClasses:
-			'hover:text-christi-600 dark:hover:text-christi-400 hover:bg-christi-50 dark:hover:bg-christi-950/50',
-		gradientClasses: 'from-christi-500/10 to-christi-600/10',
-	},
-	{
-		name: 'Discord',
-		href: 'https://discord.gg/ahKR75hU9h',
-		icon: 'streamline-logos:discord-logo-2-solid',
-		hoverClasses:
-			'hover:bg-indigo-50 hover:text-indigo-600 dark:hover:bg-indigo-950/50 dark:hover:text-indigo-400',
-		gradientClasses: 'from-indigo-500/10 to-indigo-600/10',
-	},
-	{
-		name: 'GitHub',
-		href: 'https://github.com/mindenit',
-		icon: 'streamline-logos:github-logo-2-solid',
-		hoverClasses: 'hover:text-secondary-foreground hover:bg-muted-foreground/10',
-		gradientClasses: 'from-muted/50 to-muted',
-	},
-]
 </script>
 
 <template>
